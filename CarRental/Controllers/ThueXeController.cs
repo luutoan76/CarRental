216d16eedf4ec5a4d6787c38560c109f5abeb63f
luutoan76 <124024763+luutoan76@users.noreply.github.com>
2023-10-25 23:58:35 +0700

    Lịch sử đặt xe, thêm tài khoản nhân viên

---
+++ CarRental/Controllers/ThueXeController.cs
@@ -0,0 +1,48 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Controllers
+{
+    public class ThueXeController : Controller
+    {
+        private readonly ILogger<XesController> _logger;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly CarRentContext _context;
+        public ThueXeController(CarRentContext context, IWebHostEnvironment hostEnvironment, ILogger<XesController> logger)
+        {
+            _logger = logger;
+            _context = context;
+            webHostEnvironment = hostEnvironment;
+        }
+        public async Task<IActionResult> Index(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var xe = await _context.Xes
+                .Include(x => x.TenLoaiNavigation)
+                .FirstOrDefaultAsync(m => m.BienSo == id);
+            if (xe == null)
+            {
+                return NotFound();
+            }
+
+            return View(xe);
+        }
+        /*[HttpPost]
+        public Task<IActionResult> ThueXeInfo(FormCollection fc)
+        {
+
+        }*/
+    }
+}
