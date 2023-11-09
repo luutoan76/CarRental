e6c8c64fa81f8a37089cdee4b80c4ffef654741c
khaihoan2023 <khaihoan2023@gmail.com>
2023-11-09 14:58:22 +0700

    Quản lý hóa đơn

---
+++ CarRental/Controllers/QuanLyHoaDonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarRental.Models;
+
+namespace CarRental.Controllers
+{
+    public class QuanLyHoaDonController : Controller
+    {
+        private readonly CarRentContext _context;
+
+        public QuanLyHoaDonController(CarRentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DatXes.ToListAsync());
+        }
+    }
+}
